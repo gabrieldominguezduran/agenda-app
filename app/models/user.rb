d9eda1ff13cb95d22b0dfccf96e829f2e6f44ab2
gabrieldominguezduran <gabrieldominguezduran@gmail.com>
2021-09-28 19:49:26 +0200

    Create test user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :confirmable, :lockable, :timeoutable, :trackable, :recoverable and :omniauthable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
 end
